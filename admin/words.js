7eae8825356a7cb9fe84cd751fe55d0e2d58affa
TA2k <tombox2020@gmail.com>
2022-01-24 22:43:20 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "parcel adapter settings": {
+        en: "Adapter settings for parcel",
+        de: "Adaptereinstellungen für parcel",
+        ru: "Настройки адаптера для parcel",
+        pt: "Configurações do adaptador para parcel",
+        nl: "Adapterinstellingen voor parcel",
+        fr: "Paramètres d'adaptateur pour parcel",
+        it: "Impostazioni dell'adattatore per parcel",
+        es: "Ajustes del adaptador para parcel",
+        pl: "Ustawienia adaptera dla parcel",
+        "zh-cn": "parcel的适配器设置",
+    },
+    "App Email": {
+        en: "App Email",
+        de: "App-E-Mail",
+        ru: "Электронная почта приложения",
+        pt: "Email do aplicativo",
+        nl: "App-e-mail",
+        fr: "Courriel de l'application",
+        it: "E-mail dell'app",
+        es: "Correo electrónico de la aplicación",
+        pl: "E-mail aplikacji",
+        "zh-cn": "应用电子邮件",
+    },
+    "App Password": {
+        en: "App Password",
+        de: "App-Passwort",
+        ru: "Пароль приложения",
+        pt: "Senha de app",
+        nl: "App-wachtwoord",
+        fr: "Mot de passe de l'application",
+        it: "Password dell'app",
+        es: "Contraseña de la aplicación",
+        pl: "Hasło do aplikacji",
+        "zh-cn": "应用密码",
+    },
+    "Update interval (in minutes)": {
+        en: "Update interval (in minutes)",
+        de: "Aktualisierungsintervall (in Minuten)",
+        ru: "Интервал обновления (в минутах)",
+        pt: "Intervalo de atualização (em minutos)",
+        nl: "Update-interval (in minuten)",
+        fr: "Intervalle de mise à jour (en minutes)",
+        it: "Intervallo di aggiornamento (in minuti)",
+        es: "Intervalo de actualización (en minutos)",
+        pl: "Interwał aktualizacji (w minutach)",
+        "zh-cn": "更新间隔（分钟）",
+    },
+};
---
+++ main.js
@@ -0,0 +1,342 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.0.2
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const Json2iob = require("./lib/json2iob");
+const tough = require("tough-cookie");
+const { HttpsCookieAgent } = require("http-cookie-agent");
+
+class Parcel extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "parcel",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+
+        this.json2iob = new Json2iob(this);
+        this.sessions = {};
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+
+        this.cookieJar = new tough.CookieJar();
+        this.requestClient = axios.create({
+            jar: this.cookieJar,
+            withCredentials: true,
+            httpsAgent: new HttpsCookieAgent({
+                jar: this.cookieJar,
+            }),
+        });
+
+        this.updateInterval = null;
+        this.reLoginTimeout = null;
+        this.refreshTokenTimeout = null;
+        this.subscribeStates("*");
+
+        if (this.config.dhlusername && this.config.dhlpassword) {
+            this.log.info("Login to DHL");
+            await this.loginDHL();
+        }
+
+        if (Object.keys(this.sessions).length > 0) {
+            await this.updateProvider();
+            this.updateInterval = setInterval(async () => {
+                await this.updateProvider();
+            }, this.config.interval * 60 * 1000);
+            this.refreshTokenInterval = setInterval(() => {
+                this.refreshToken();
+            }, 3500 * 1000);
+        } else {
+            this.log.warn("No login session found");
+        }
+    }
+    async loginDHL() {
+        await this.requestClient({
+            method: "get",
+            url: "https://www.dhl.de/int-webapp/spa/prod/ver4-SPA-VERFOLGEN.html?adobe_mc=TS%3D1643057331%7CMCORGID%3D3505782352FCE66F0A490D4C%40AdobeOrg",
+            headers: {
+                accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                "accept-language": "de-de",
+            },
+            jar: this.cookieJar,
+            withCredentials: true,
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+        await this.requestClient({
+            method: "get",
+            url: "https://www.dhl.de/int-erkennen/deviceid",
+            headers: {
+                accept: "*/*",
+                "accept-language": "de-de",
+                "x-requested-with": "XMLHttpRequest",
+                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                referer: "https://www.dhl.de/int-webapp/spa/prod/ver4-SPA-VERFOLGEN.html?adobe_mc=TS%3D1643059174%7CMCORGID%3D3505782352FCE66F0A490D4C%40AdobeOrg",
+            },
+            jar: this.cookieJar,
+            withCredentials: true,
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+        await this.requestClient({
+            method: "post",
+            url: "https://www.dhl.de/int-erkennen/refresh",
+            headers: {
+                Host: "www.dhl.de",
+                "content-type": "application/json",
+                accept: "*/*",
+                "x-requested-with": "XMLHttpRequest",
+                "accept-language": "de-de",
+                origin: "https://www.dhl.de",
+                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                referer: "https://www.dhl.de/int-webapp/spa/prod/ver4-SPA-VERFOLGEN.html?adobe_mc=TS%3D1643039135%7CMCORGID%3D3505782352FCE66F0A490D4C%40AdobeOrg",
+            },
+            data: JSON.stringify({
+                force: false,
+                meta: "",
+            }),
+            jar: this.cookieJar,
+            withCredentials: true,
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+        await this.requestClient({
+            method: "post",
+            url: "https://www.dhl.de/int-erkennen/login",
+            headers: {
+                Host: "www.dhl.de",
+                "content-type": "application/json",
+                accept: "*/*",
+                "x-requested-with": "XMLHttpRequest",
+                "accept-language": "de-de",
+                origin: "https://www.dhl.de",
+                "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+            },
+            jar: this.cookieJar,
+            withCredentials: true,
+            data: JSON.stringify({
+                id: this.config.dhlusername,
+                password: this.config.dhlpassword,
+                authenticationLevel: 3,
+                authenticationMethod: ["pwd"],
+                rememberMe: true,
+                language: "de",
+                context: "app",
+                meta: "",
+            }),
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.sessions["dhl"] = res.data;
+                this.setState("info.connection", true, true);
+                await this.setObjectNotExistsAsync("dhl", {
+                    type: "device",
+                    common: {
+                        name: "DHL Tracking",
+                    },
+                    native: {},
+                });
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+    }
+
+    async updateProvider() {
+        const statusArrays = {
+            dhl: [
+                {
+                    path: ".dhl",
+                    url: "https://www.dhl.de/int-verfolgen/data/search?merge=false&noRedirect=true&language=de&cid=app",
+                    header: {
+                        accept: "application/json",
+                        "content-type": "application/json",
+                        "verfolgen-csrf-token": "363d8052-1cb4-43c8-840c-e15e65ebba55",
+                        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                        referer: "https://www.dhl.de/int-webapp/spa/prod/ver4-SPA-VERFOLGEN.html?adobe_mc=TS%3D1643039135%7CMCORGID%3D3505782352FCE66F0A490D4C%40AdobeOrg",
+                        "accept-language": "de-de",
+                    },
+                },
+            ],
+        };
+
+        for (const id of Object.keys(this.sessions)) {
+            for (const element of statusArrays[id]) {
+                await this.requestClient({
+                    method: "get",
+                    url: element.url,
+                    headers: element.header,
+                    jar: this.cookieJar,
+                    withCredentials: true,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        if (!res.data) {
+                            return;
+                        }
+                        const data = res.data;
+
+                        const forceIndex = null;
+                        const preferedArrayName = null;
+
+                        this.json2iob.parse(element.path, data, { forceIndex: forceIndex, preferedArrayName: preferedArrayName });
+                    })
+                    .catch((error) => {
+                        if (error.response) {
+                            if (error.response.status === 401) {
+                                error.response && this.log.debug(JSON.stringify(error.response.data));
+                                this.log.info(element.path + " receive 401 error. Refresh Token in 60 seconds");
+                                if (!this.refreshTokenTimeout) {
+                                    this.refreshTokenTimeout = setTimeout(() => {
+                                        this.refreshTokenTimeout = null;
+                                        this.refreshToken(id);
+                                    }, 1000 * 60);
+                                }
+                                return;
+                            }
+                        }
+                        this.log.error(element.url);
+                        this.log.error(error);
+                        error.response && this.log.error(JSON.stringify(error.response.data));
+                    });
+            }
+        }
+    }
+    async refreshToken(id) {
+        if (this.sessions[id]) {
+            this.log.error("No session found relogin");
+
+            return;
+        }
+        for (const id of Object.keys(this.sessions)) {
+            if (id === "dhl") {
+                await this.requestClient({
+                    method: "post",
+                    url: "https://www.dhl.de/int-erkennen/refresh",
+                    jar: this.cookieJar,
+                    withCredentials: true,
+                    headers: {
+                        Host: "www.dhl.de",
+                        "content-type": "application/json",
+                        accept: "*/*",
+                        "x-requested-with": "XMLHttpRequest",
+                        "accept-language": "de-de",
+                        origin: "https://www.dhl.de",
+                        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                        referer: "https://www.dhl.de/",
+                    },
+                    data: JSON.stringify({
+                        force: false,
+                        meta: this.sessions["dhl"].meta,
+                    }),
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        this.sessions["id"] = res.data;
+                        this.setState("info.connection", true, true);
+                    })
+                    .catch((error) => {
+                        this.log.error("refresh token failed");
+                        this.log.error(error);
+                        error.response && this.log.error(JSON.stringify(error.response.data));
+                        this.log.error("Start relogin in 1min");
+                        if (!this.reLoginTimeout) {
+                            this.reLoginTimeout = setTimeout(() => {
+                                this.reLoginTimeout = null;
+                                this.loginDHL();
+                            }, 1000 * 60 * 1);
+                        }
+                    });
+            }
+        }
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            this.setState("info.connection", false, true);
+            this.reLoginTimeout && clearTimeout(this.reLoginTimeout);
+            this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+            this.updateInterval && clearInterval(this.updateInterval);
+            this.refreshTokenInterval && clearInterval(this.refreshTokenInterval);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    async onStateChange(id, state) {
+        if (state) {
+            if (!state.ack) {
+                if (id.split(".")[2] !== "refresh") {
+                    this.updateProvider();
+                }
+            }
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Parcel(options);
+} else {
+    // otherwise start the instance directly
+    new Parcel();
+}
