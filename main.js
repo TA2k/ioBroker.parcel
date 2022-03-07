8a9f3d23b8c32b4df17f43a4f0e8a78de91e8159
TA2k <tombox2020@gmail.com>
2022-03-07 21:08:45 +0100

    fix var

--- main.js
+++ main.js
@@ -1240,7 +1240,7 @@ class Parcel extends utils.Adapter {
             for (const sendung of data.sendungen) {
                 sendung.source = "DPD";
                 sendung.inDelivery = this.inDeliveryCheck(sendung);
-                sendung.delivered = this.deliveryStatusCheck(sendung, id, sendungsObject);
+                sendung.delivered = this.deliveryStatusCheck(sendung, id, sendung);
 
                 this.mergedJsonObject[sendung.id] = sendung;
             }
