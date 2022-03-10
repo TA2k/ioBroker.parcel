d8f74e66b7f39ee2b975df4d2313dcd8eae0b140
gerbuchner <36327853+gerbuchner@users.noreply.github.com>
2022-03-10 07:49:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ class Parcel extends utils.Adapter {
         this.images = {};
         this.alreadySentMessages = {};
         this.firstStart = true;
-        this.delivery_status = { ERROR: -1, UNKNOWN: 5, REGISTERED: 10, OUT_FOR_DELIVERY: 20, IN_TRANSIT: 30, IN_PREPARATION: 40, DELIVERED: 0 };
+        this.delivery_status = { ERROR: -1, UNKNOWN: 5, REGISTERED: 10, OUT_FOR_DELIVERY: 20, IN_TRANSIT: 30, IN_PREPARATION: 40, DELIVERED: 1 };
     }
 
     /**
@@ -1352,25 +1352,25 @@ class Parcel extends utils.Adapter {
         try {
             if (sendung) {
                 if (id === "dhl" && sendung.sendungsdetails && sendung.sendungsdetails.sendungsverlauf && sendung.sendungsdetails.sendungsverlauf.fortschritt) {
-                    const dhl_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, 5: 0 };
+                    const dhl_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, 5: 1 };
                     if (dhl_status[sendung.sendungsdetails.sendungsverlauf.fortschritt] !== undefined) {
                         return dhl_status[sendung.sendungsdetails.sendungsverlauf.fortschritt];
                     }
                 }
                 if (id === "hermes" && sendung.lastStatusId) {
-                    const hermes_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, 5: 0 };
+                    const hermes_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, 5: 1 };
                     if (hermes_status[sendung.lastStatusId] !== undefined) {
                         return hermes_status[sendung.lastStatusId];
                     }
                 }
                 if (id === "gls" && sendung.status) {
-                    const gls_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, DELIVERED: 0 };
+                    const gls_status = { 0: 10, 1: 10, 2: 20, 3: 30, 4: 40, DELIVERED: 1 };
                     if (gls_status[sendung.lastStatusId] !== undefined) {
                         return gls_status[sendung.lastStatusId];
                     }
                 }
                 if (id === "amz" && sendung.detailedState && sendung.detailedState.progressTracker && sendung.detailedState.progressTracker.numberOfReachedMilestones) {
-                    const amz_status = { 0: 10, 1: 10, 2: 30, 3: 40, 4: 0 };
+                    const amz_status = { 0: 10, 1: 10, 2: 30, 3: 40, 4: 1 };
                     if (amz_status[sendung.detailedState.progressTracker.numberOfReachedMilestones] !== undefined) {
                         return amz_status[sendung.detailedState.progressTracker.numberOfReachedMilestones];
                     }
