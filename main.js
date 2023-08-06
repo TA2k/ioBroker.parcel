1461406c7a420b32a1efcc40a9f5c565f8dccfcc
TA2k <tombox2020@gmail.com>
2023-08-07 01:53:47 +0200

    add amz headers

--- main.js
+++ main.js
@@ -2214,9 +2214,15 @@ class Parcel extends utils.Adapter {
       method: "get",
       url: "https://www.amazon.de/gp/css/order-history?ref_=nav_orders_first&disableCsd=missing-library",
       headers: {
-        accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        authority: "www.amazon.de",
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 
         "accept-language": "de-de",
+        "cache-control": "max-age=0",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "viewport-width": "1272",
       },
       jar: this.cookieJar,
       withCredentials: true,
